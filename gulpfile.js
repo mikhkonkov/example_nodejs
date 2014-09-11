d71a3fb6a7ebe4731243d668506be2d2e0bd6622
Dementev Roman <infud@yandex.ru>
2014-09-12 00:30:47 +0400

    server work almost but looks bad

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+/* global require */
+
+var path = require("path");
+
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var webpack = require("webpack");
+var webpackConfig = require("./webpack.config.js");
+var WebpackDevServer = require("webpack-dev-server");
+
+gulp.task("dev-server", function() {
+  var compiler = webpack(webpackConfig);
+  new WebpackDevServer(compiler, {
+        // server and middleware options
+    }).listen(8080, "localhost", function(err) {
+        if(err) throw new gutil.PluginError("webpack-dev-server", err);
+        // Server listening
+        gutil.log("[webpack-dev-server]", "http://localhost:8080/src/index.html");
+
+        // keep the server alive or continue?
+        // callback();
+    });
+});
